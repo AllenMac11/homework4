c75ce676fb85750f8e5507427473c3af34f828cc
Allen Mac <mparmar@njcu.edu>
2020-05-27 17:57:44 +0000

    Second commit

--- first.java
+++ first.java
@@ -1,6 +1,6 @@
 public class first{
 	public static void main(String[] args){
-	System.out.println("My name is Allen Mac");
+	System.out.println("my favorite progrraming language is JAVA");
 	}
 }
 
